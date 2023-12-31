13a5627b9356ecfd5de8145fe15aa23ce1928b07
WarrenE456 <w@eavesonline.com>
2023-12-31 10:10:46 -0600

    Add nd rot matricies, ncube vert calculation and line index calculation

--- ncube.cpp
+++ ncube.cpp
@@ -10,43 +10,15 @@ using std::array;
 using std::sin;
 using std::cos;
 
+static vector<vector<double>> matmul(vector<vector<double>> A, vector<vector<double>> B);
+static void draw_line(double x1, double y1, double x2, double y2, char* screen, int width, int height);
+static void bianary_purmutations(vector<vector<vector<double>>>& verts, int n);
+
 const char CLEAR = ' ', DRAW = '*'; // clear and draw character
 const double T = .1; // rotation amount on every axis
 const double CAMERA_DISTANCE = 1.25; // distance camera is away from cube
 
-vector<vector<double>> matmul(vector<vector<double>> A, vector<vector<double>> B);
-void draw_line(double x1, double y1, double x2, double y2, char* screen, int width, int height);
-
-// rortation matricies multiplied to make the tranformation matrix R that rotates on each axis
-vector<vector<double>> Rx = {{cos(T), -sin(T), 0}, {sin(T), cos(T), 0}, {0, 0, 1}};
-vector<vector<double>> Ry = {{1, 0, 0}, {0, cos(T), -sin(T)}, {0, sin(T), cos(T)}};
-vector<vector<double>> Rz = {{cos(T), 0, sin(T)}, {0, 1, 0}, {-sin(T), 0, cos(T)}};
-vector<vector<double>> R = matmul(Rx, matmul(Ry, Rz));
-
 int main(void) {
-    vector<vector<vector<double>>> verts; // array of verticies stored as 3d vectors / 3 x 1 matricies
-    // generate verticies (+-0.5, +-0.5, +-0.5)
-    for (int i = -1; i <= 1; i += 2) {
-        for (int j = -1; j <= 1; j += 2) {
-            for (int k = -1; k <= 1; k += 2) {
-                vector<vector<double>> vert = {{double(i) / 2}, {double(j) / 2}, {double(k) / 2}};
-                verts.push_back(vert);
-            }
-        }
-    }
-    vector<array<int, 2>> line_indicies; // vector of all indicies to connect to create each line
-    // find each index by searching for what pairs make a line of length 1
-    for (int i = 0; i < verts.size(); i++) {
-        for (int j = i + 1; j < verts.size(); j++) {
-            int delta_x, delta_y, delta_z;
-            delta_x = abs(verts[i][0][0] - verts[j][0][0]);
-            delta_y = abs(verts[i][1][0] - verts[j][1][0]);
-            delta_z = abs(verts[i][2][0] - verts[j][2][0]);
-            if (.99 <= delta_x + delta_y + delta_z && delta_x + delta_y + delta_z <= 1.01) {
-                line_indicies.push_back({i, j});
-            }
-        }
-    }
     // get the resulotion from the user
     int width, height;
     std::string temp_s;
@@ -59,7 +31,7 @@ int main(void) {
             width = temp_i;
             break;
         }
-        std::cout << "Must be greater than 0.\n";
+        std::cout << "\nMust be greater than 0.\n";
     }
     while (true) {
         std::cout << "Enter screen height: ";
@@ -69,7 +41,48 @@ int main(void) {
             height = temp_i;
             break;
         }
-        std::cout << "Must be greater than 0.\n";
+        std::cout << "\nMust be greater than 0.\n";
+    }
+    int n;
+    // get number of dimensions from user
+    while (true) {
+        std::cout << "Enter dimension count: ";
+        std::cin >> temp_s;
+        temp_i = std::atoi(temp_s.c_str());
+        if (temp_i >= 2) {
+            n = temp_i;
+            break;
+        }
+        std::cout << "\nDimension count must be atleast 2.\n";
+    }
+    // initlaize the rotation matrix;
+    vector<vector<double>> I(n, vector<double>(n, 0));
+    for (int i = 0; i < n; i++) I[i][i] = 1;  
+    vector<vector<double>> R = I;
+    vector<vector<double>> Rn;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            Rn = I;
+            Rn[i][i] = cos(T);
+            Rn[i][j] = -sin(T);
+            Rn[j][i] = sin(T);
+            Rn[j][j] = cos(T);
+            R = matmul(R, Rn);
+        }
+    }
+    vector<vector<vector<double>>> verts; // array of verticies stored as nd vectors / n x 1 matricies
+    // generate verticies (+-0.5, +-0.5,..., +-0.5)
+    bianary_purmutations(verts, n);
+    vector<array<int, 2>> line_indicies; // vector of all indicies to connect to create each line
+    // find each index by searching for what pairs make a line of length 1
+    for (int i = 0; i < verts.size(); i++) {
+        for (int j = i + 1; j < verts.size(); j++) {
+            double len = 0;
+            for (int k = 0; k < n; k++) {
+                len += abs(verts[i][k][0] - verts[j][k][0]);
+            }
+            if (std::round(len) == 1) line_indicies.push_back({i, j});
+        }
     }
     // initialize screen
     // screen at (x, y) -> screen[x + (width + 1) * y]
@@ -111,7 +124,7 @@ int main(void) {
 }
 
 // matrix multiplication
-vector<vector<double>> matmul(vector<vector<double>> A, vector<vector<double>> B) {
+static vector<vector<double>> matmul(vector<vector<double>> A, vector<vector<double>> B) {
     int n = A.size();
     int m = B[0].size();
     int common = A[0].size();
@@ -128,7 +141,7 @@ vector<vector<double>> matmul(vector<vector<double>> A, vector<vector<double>> B
 
 // takes cordnates and draws a line between the points on the m x n screen
 // give cordinates as follows: 0 <= x < n, m and 0 <= y < n, m
-void draw_line(double x1, double y1, double x2, double y2, char* screen, int width, int height) {
+static void draw_line(double x1, double y1, double x2, double y2, char* screen, int width, int height) {
     int step_count = (int)std::round(std::sqrt(std::pow(x2-x1, 2) + std::pow(y2-y1, 2)));
     if (step_count == 0) return;
     double dx = (x2 - x1) / step_count, dy = (y2 - y1) / step_count;
@@ -140,4 +153,18 @@ void draw_line(double x1, double y1, double x2, double y2, char* screen, int wid
         if (!(0 <= y && y < height)) return;
         screen[x + (width + 1) * y] = DRAW;
     }
+    return;
+}
+
+static void bianary_purmutations(vector<vector<vector<double>>>& verts, int n) {
+    int p = 1 << n;
+    for (int i = 0; i < p; i++) {
+        vector<vector<double>> a;
+        for (int j = 0; j < n; j++) {
+            a.push_back({(double)(i >> j & 1)});
+            a.back()[0] -= .5;
+        }
+        verts.push_back(a);
+    }
+    return;
 }
